fc030748c985c3476dbc39727326748e5bd2b1b7
hojncode <glicm12@gmail.com>
2023-06-23 16:25:16 +0900

    FEAT : 스프링 빈을 등록하기 - 컴포넌트 스캔과 자동 의존관계 설정 (어노테이션 추가로 스프링 빈에 등록한다)

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -3,11 +3,14 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 //회원 서비스 로직 ( 비지니스 로직을 처리하도록 설계 )
+@Service
 public class MemberService {
 
 //    private final MemberRepository memberRepository = new MemoryMemberRepository();
@@ -15,7 +18,9 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
     // 외부에서 레포지토리를 넣어준다. (테스트에 있는 레포지토리와 동일한 레포지토리를 사용하기위함)
+        @Autowired
         public MemberService(MemberRepository memberRepository) {
+
             this.memberRepository = memberRepository;
         }
 
