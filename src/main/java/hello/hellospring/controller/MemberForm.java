f6f5e47e8f7e3d3d7647fe92b6f2277030efd0c0
hojncode <glicm12@gmail.com>
2023-06-23 16:26:19 +0900

    FEAT : 회원 정보 입력, 저장, 리스트 불러오기

---
+++ src/main/java/hello/hellospring/controller/MemberForm.java
@@ -0,0 +1,13 @@
+package hello.hellospring.controller;
+
+public class MemberForm {
+    private String name;
+
+    public String getName() { // getter
+        return name;
+    }
+
+    public void setName(String name) { // setter
+        this.name = name;
+    }
+}
